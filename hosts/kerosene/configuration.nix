3f719e50ed6fae6154ff536f17dc5cd6b7a9b126
Erik Karsten <erik@opcode.xyz>
2022-01-04 19:17:03 +0000

    Add new host kerosene

---
+++ hosts/kerosene/configuration.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+
+      # Other Modules
+      ../../modules/system/env.nix
+      ../../modules/system/fonts.nix
+      ../../modules/system/sound.nix
+      ../../modules/system/xorg.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  time.timeZone = "Europe/Helsinki";
+
+  networking = {
+    hostName = "kerosene";
+    networkmanager.enable = true;
+    useDHCP = false;
+    interfaces.enp0s31f6.useDHCP = true;
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  programs.gnupg.agent.enable = true;
+
+  users.users.shebpamm = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableGlobalCompInit = false;
+  };
+
+  environment.binsh = "${pkgs.dash}/bin/dash";
+
+  environment.systemPackages = with pkgs; [
+    coreutils
+    gcc
+    gnupg
+    pinentry
+    usbutils
+    vim
+    zsh
+  ];
+
+  nix = {
+    package = pkgs.nixUnstable;
+    trustedUsers = [ "root" "shebpamm" "@wheel" ];
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  system.stateVersion = "21.11";
+}
