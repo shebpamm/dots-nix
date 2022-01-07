7e230e08755704e985c59955da967da2c13bfd25
shebpamm <erik@opcode.xyz>
2022-01-07 11:29:09 +0200

    Update awesomewm to nord-theme named lake

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -3,6 +3,7 @@ pcall(require, "luarocks.loader")
 require "awful.autofocus"
 local beautiful = require "beautiful"
 local naughty = require "naughty"
+local gears = require "gears"
 
 -- err handling
 naughty.connect_signal("request::display_error", function(message, startup)
@@ -14,10 +15,13 @@ naughty.connect_signal("request::display_error", function(message, startup)
 end)
 
 -- truly beautiful
-beautiful.init(require("gears").filesystem.get_configuration_dir() .. "themes/forest/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/lake/theme.lua")
 
 -- *gulp*
 require("modules.bling").module.window_swallowing.start()
+
+-- wallpaper
+require "wallpaper"
 
 -- config stuff
 require "configuration"
