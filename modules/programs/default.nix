240aca2b73d8c15a51098d5b4f143fa15224b760
shebpamm <erik@opcode.xyz>
2022-01-07 16:36:58 +0200

    Add discord to packages

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, libs, ... }:
 {
   imports = [ ./irssi.nix ./kitty ./spotify.nix ];
-  home.packages = with pkgs; [ google-chrome rofi nyxt ];
+  home.packages = with pkgs; [ google-chrome rofi nyxt discord ];
   home.file.".config/rofi".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/rofi";
 }
