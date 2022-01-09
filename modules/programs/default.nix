c8bccee5afa7b81ca303e9212a5c45208540b0a7
shebpamm <erik@opcode.xyz>
2022-01-09 12:26:54 +0200

    Add some fun programs such as lolcat

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,6 +1,6 @@
 { config, pkgs, libs, ... }:
 {
-  imports = [ ./irssi.nix ./kitty ./spotify.nix ];
+  imports = [ ./irssi.nix ./kitty ./spotify.nix ./fun.nix ];
   home.packages = with pkgs; [ google-chrome rofi nyxt discord ];
   home.file.".config/rofi".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/rofi";
