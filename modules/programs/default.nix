0e55910be2ecad0da7df8e96d4f79ed6c201e223
shebpamm <erik@opcode.xyz>
2022-01-09 15:00:38 +0200

    add openrgb as software

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,7 +1,13 @@
 { config, pkgs, libs, ... }:
 {
   imports = [ ./irssi.nix ./kitty ./spotify.nix ./fun.nix ];
-  home.packages = with pkgs; [ google-chrome rofi nyxt discord ];
+  home.packages = with pkgs; [ 
+    google-chrome
+    rofi
+    nyxt
+    discord
+    openrgb
+  ];
   home.file.".config/rofi".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/rofi";
 }
