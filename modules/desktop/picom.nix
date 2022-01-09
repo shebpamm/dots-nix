d5a0abe7d6e6a8186bdab4be1927f0f60b762bec
shebpamm <erik@opcode.xyz>
2022-01-09 10:37:13 +0200

    Recreate picom.conf as a nix expression

--- modules/desktop/picom.nix
+++ modules/desktop/picom.nix
@@ -1,5 +1,39 @@
 { config, pkgs, libs, ... }:
 {
-  home.packages = [ pkgs.picom-git ];
+  services.picom = {
+      enable = true;
+      package = pkgs.picom-pijulius;
+      
+      shadow = true;
+      shadowOpacity = "0.4";
+      shadowOffsets = [ (-5) (0) ];
+      shadowExclude = [
+        "name = 'Notification'"
+        "class_g = 'Conky'"
+        "class_g ?= 'Notify-osd'"
+        "class_g = 'Cairo-clock'"
+        "class_g = 'awesome'"
+        "_GTK_FRAME_EXTENTS@:c"
+      ];
+
+      fade = true;
+      fadeDelta = 3;
+      fadeSteps = [ "0.03" "0.03" ];
+      
+      backend = "glx";
+
+      vSync = true;
+
+      extraOptions = ''
+        shadow-radius = 16;
+        corner-radius = 20;
+        rounded-corners-exclude = [
+          "window_type = 'dock'",
+          "window_type = 'desktop'",
+          "class_g = 'awesome'"
+        ];
+      '';
+    };
+  # home.packages = [ pkgs.picom-pijulius ];
   home.file.".config/picom".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/picom";
 }
