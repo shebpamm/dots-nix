016e6a0d1a05784284708d9c0714e70d2acaab98
shebpamm <erik@opcode.xyz>
2022-01-20 19:31:27 +0200

    Change gtk theme to Nordic

--- modules/desktop/gtk.nix
+++ modules/desktop/gtk.nix
@@ -1,13 +1,13 @@
 { config, pkgs, libs, ... }:
 {
-  home.packages = [ pkgs.lounge-gtk-theme pkgs.paper-icon-theme ];
+  home.packages = [ pkgs.nordic pkgs.paper-icon-theme ];
   gtk = {
     enable = true;
     font = {
       name = "JetBrainsMono Nerd Font";
       size = 10;
     };
-    theme.name = "Lounge-night-compact";
+    theme.name = "Nordic";
     iconTheme.name = "Paper";
   };
 }
