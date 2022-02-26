83e04b9944961fcc02a96b72506b7e474688a6d4
shebpamm <erik@opcode.xyz>
2022-02-26 16:41:06 +0200

    add alias for rebooting straight into windows

--- config/zsh/.zsh/aliases.zsh
+++ config/zsh/.zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias li="exa --icons"
 alias lg="lazygit"
 alias btctl="bluetoothctl"
 alias grep="grep --color=auto"
+alias reboot-windows="sudo nix-shell -p efibootmgr --run 'efibootmgr -n 0000' && reboot"
 
 # replace AUTO_CD
 alias ~="cd ~"
