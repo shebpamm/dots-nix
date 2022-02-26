9782dbdbff3936c61960f5d0e09711a9304c8511
shebpamm <erik@opcode.xyz>
2022-02-26 16:39:27 +0200

    update configuration to use new nix.settings option

--- hosts/kerosene/configuration.nix
+++ hosts/kerosene/configuration.nix
@@ -90,18 +90,20 @@
 
   nix = {
     package = pkgs.nixUnstable;
-    trustedUsers = [ "root" "shebpamm" "@wheel" ];
-    binaryCaches = [
-      "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
-      "https://fortuneteller2k.cachix.org"
-    ];
+    settings = {
+      trusted-users = [ "root" "shebpamm" "@wheel" ];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "https://fortuneteller2k.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "fortuneteller2k.cachix.org-1:kXXNkMV5yheEQwT0I4XYh1MaCSz+qg72k8XAi2PthJI="
+      ];
+    };
 
-    binaryCachePublicKeys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "fortuneteller2k.cachix.org-1:kXXNkMV5yheEQwT0I4XYh1MaCSz+qg72k8XAi2PthJI="
-    ];
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
