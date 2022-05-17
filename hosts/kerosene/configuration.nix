fd7d89b4bab8244ffe48a28fd876ab74235f91a5
shebpamm <erik@opcode.xyz>
2022-05-17 21:41:28 +0300

    add xorg-nvidia back and move xrandr to hosts/kerosene

--- hosts/kerosene/configuration.nix
+++ hosts/kerosene/configuration.nix
@@ -67,7 +67,21 @@
   };
 
 
-  displayManager.sessionCommands = ''${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-0 --off --output DP-0 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --rate 240 --pos 2160x1236 --rotate normal --output DP-3 --off --output HDMI-1 --off --output USB-C-0 --off'';
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+
+    displayManager.sessionCommands = ''${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-0 --off --output DP-0 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --rate 240 --pos 2160x1236 --rotate normal --output DP-3 --off --output HDMI-1 --off --output USB-C-0 --off'';
+
+    xrandrHeads = [
+      { output = "DP-2"; monitorConfig = ''
+        Option "Position" "2160 1236"
+      ''; }
+      { output = "DP-0"; monitorConfig = ''
+        Option "Rotate" "left"
+        Option "Position" "0 0"
+      ''; }
+    ];
+  };
 
   environment.binsh = "${pkgs.dash}/bin/dash";
 
