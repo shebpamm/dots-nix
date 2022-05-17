c76a41c0803cdb6ff87728928ba541546379b0af
shebpamm <erik@opcode.xyz>
2022-05-17 19:28:23 +0300

    add bitwarden-cli and desktop

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,12 +1,14 @@
 { config, pkgs, libs, ... }:
 {
-  imports = [ ./irssi.nix ./kitty ./fun.nix ];
+  imports = [ ./irssi.nix ./kitty ./fun.nix ./spotify.nix ];
   home.packages = with pkgs; [ 
     google-chrome
     rofi
     nyxt
     discord
     kitty
+    bitwarden
+    bitwarden-cli
   ];
   home.file.".config/rofi".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/rofi";
