3aa3432201d8ad37aed88f443e00d62656c6f58f
shebpamm <erik@opcode.xyz>
2022-05-28 18:55:16 +0300

    add EDITOR to nvim

--- modules/system/env.nix
+++ modules/system/env.nix
@@ -5,6 +5,7 @@
     XDG_CONFIG_HOME = "\${HOME}/.config";
     XDG_BIN_HOME = "\${HOME}/.local/bin";
     XDG_DATA_HOME = "\${HOME}/.local/share";
+    EDITOR = "nvim";
 
     PATH = [
       "\${HOME}/.bin"
