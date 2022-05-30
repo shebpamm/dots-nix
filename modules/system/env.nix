05349e439f23e6a8fce452db8d0774aadcca46b9
shebpamm <erik@opcode.xyz>
2022-05-30 16:17:31 +0300

    fix session vars not working

--- modules/system/env.nix
+++ modules/system/env.nix
@@ -1,16 +1,16 @@
 { config, pkgs, ... }:
 {
   environment.sessionVariables = rec {
-    XDG_CACHE_HOME = "\${HOME}/.cache";
-    XDG_CONFIG_HOME = "\${HOME}/.config";
-    XDG_BIN_HOME = "\${HOME}/.local/bin";
-    XDG_DATA_HOME = "\${HOME}/.local/share";
+    XDG_CACHE_HOME = "\$HOME/.cache";
+    XDG_CONFIG_HOME = "\$HOME/.config";
+    XDG_BIN_HOME = "\$HOME/.local/bin";
+    XDG_DATA_HOME = "\$HOME/.local/share";
     EDITOR = "nvim";
 
     PATH = [
-      "\${HOME}/.bin"
-      "\${XDG_BIN_HOME}"
-      "\${HOME}/.node_modules"
+      "\$HOME/.bin"
+      "\$XDG_BIN_HOME"
+      "\$HOME/.node_modules"
     ];
   };
 }
