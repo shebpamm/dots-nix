19fa38d0d67ce38f900c873da72df3728db39553
shebpamm <erik@opcode.xyz>
2022-06-01 13:20:36 +0300

    change greeter to lightdm

--- modules/system/xorg.nix
+++ modules/system/xorg.nix
@@ -2,17 +2,20 @@
 {
   services.xserver = {
     enable = true;
-    # displayManager.lightdm = {
-    #   enable = true;
-    #   greeters.gtk.enable = true;
-    # };
-    displayManager.gdm = {
-      enable = true;
-      wayland = false;
-    };
+     displayManager.lightdm = {
+       enable = true;
+#       greeters.gtk.enable = true;
+     };
+    #displayManager.gdm = {
+    #  enable = true;
+    #  wayland = false;
+    #};
 
 #    displayManager.sessionCommands = ''${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-0 --off --output DP-0 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --rate 240 --pos 2160x1236 --rotate normal --output DP-3 --off --output HDMI-1 --off --output USB-C-0 --off'';
 
+    displayManager = {
+      defaultSession = "none+awesome";
+    };
 
     libinput.enable = true;
     layout = "eu";
@@ -31,7 +34,7 @@
   };
 
   programs.sway = {
-    enable = true;
+    enable = false;
     wrapperFeatures.gtk = true;
   };
 }
