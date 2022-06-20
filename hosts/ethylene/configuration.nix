1cfd46600f3ea3820d147617f24b2acf25d9f3e0
shebpamm <erik@opcode.xyz>
2022-06-20 19:15:58 +0300

    add all kinds of printing drivers

--- hosts/ethylene/configuration.nix
+++ hosts/ethylene/configuration.nix
@@ -88,6 +88,15 @@
 
   };
 
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cnijfilter2
+      gutenprintBin
+      canon-cups-ufr2
+    ];
+  };
+
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
