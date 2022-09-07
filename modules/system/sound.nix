2261dd3244e4bb0dd98818a10b5992fdc5884a37
shebpamm <erik@opcode.xyz>
2022-09-07 11:37:34 +0300

    add spotifyd without config

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -3,6 +3,10 @@
   sound.enable = true;
   #hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
+  services.spotifyd = {
+    enable = true;
+  };
+
   services.pipewire = {
     enable = true;
     alsa = {
