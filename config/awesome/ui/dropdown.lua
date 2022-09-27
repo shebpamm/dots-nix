c1fa2519f59673d657ac60fc7e2f62ca6b89cd1b
shebpamm <erik@opcode.xyz>
2022-09-27 22:53:11 +0300

    make quake dropdown use named tmux session

--- config/awesome/ui/dropdown.lua
+++ config/awesome/ui/dropdown.lua
@@ -2,5 +2,5 @@ local awful = require "awful"
 local quake = require "modules.quake"
 
 awful.screen.connect_for_each_screen(function(s)
-  s.quake = quake({ app = "kitty", visible = false, height = 0.8, screen = s, extra = "-e tmux", argname="--class %s", name="quake", border=0 })
+  s.quake = quake({ app = "kitty", visible = false, height = 0.8, screen = s, extra = "tmux new-session -A -s dropdown", argname="--class %s", name="quake", border=0 })
 end)
