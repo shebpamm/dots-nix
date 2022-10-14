244af67cfa0bc48bb75f105019628614873c52c0
shebpamm <erik@opcode.xyz>
2022-10-14 16:47:15 +0300

    add blame line toggle

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -27,6 +27,7 @@ require("gitsigns").setup {
     ["v <leader>gr"] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
     ["n <leader>gR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
     ["n <leader>gp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ["n <leader>gb"] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
 
     -- Text objects
     ["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
