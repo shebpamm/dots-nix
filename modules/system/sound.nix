6ab44463a6a23bdd6f1acbb2bdcb613a48a3107a
shebpamm <erik@opcode.xyz>
2022-11-03 14:08:04 +0200

    add pipewire once again

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -1,18 +1,18 @@
 { config, pkgs, ... }:
 {
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  #hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
   services.spotifyd = {
     enable = true;
   };
 
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa = {
-  #     enable = true;
-  #     support32Bit = true;
-  #   };
-  #   pulse.enable = true;
-  # };
+  services.pipewire = {
+    enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
+  };
 }
