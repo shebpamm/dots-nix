5a9b65ad47e7af31da3c7a255e6a725bc006d107
shebpamm <erik@opcode.xyz>
2022-11-28 20:06:59 +0200

    add monitor resolution, temporary rofi bind

--- modules/desktop/windowManagers/hyprland.nix
+++ modules/desktop/windowManagers/hyprland.nix
@@ -5,7 +5,10 @@
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = ''
+      monitor=DP-1,5120x1440@120,auto,1
+
       bind=SUPER,return,exec,kitty
+      bind=SUPER,d,exec,rofi -show drun
     '';
   };
   
