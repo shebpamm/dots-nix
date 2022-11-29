28852d9dcbfdf740f11a9cf9b9a4f1eeed1d03ad
shebpamm <erik@opcode.xyz>
2022-11-29 14:40:17 +0200

    separate hyprland config to different files

--- modules/desktop/windowManagers/hyprland.nix
+++ modules/desktop/windowManagers/hyprland.nix
@@ -1,41 +1,24 @@
 { config, pkgs, libs, inputs, ... }:
+let
+  configPath = ../../../config/hypr;
+  monitors = (builtins.readFile (configPath + /monitors.conf));
+  binds = (builtins.readFile (configPath + /binds.conf));
+  animations = (builtins.readFile (configPath + /animations.conf));
+  execs = (builtins.readFile (configPath + /execs.conf));
+in
 {
   imports = [ inputs.hyprland.homeManagerModules.default ];
-
   wayland.windowManager.hyprland = {
     enable = true;
-    extraConfig = ''
-      monitor=DP-1,5120x1440@120,auto,1
-
-      bind=SUPER,return,exec,kitty
-      bind=SUPER,d,exec,rofi -show drun
-      bind=SUPER,Q,killactive,
-
-      bind=SUPER,1,workspace,1
-      bind=SUPER,2,workspace,2
-      bind=SUPER,3,workspace,3
-      bind=SUPER,4,workspace,4
-      bind=SUPER,5,workspace,5
-      bind=SUPER,6,workspace,6
-      bind=SUPER,7,workspace,7
-      bind=SUPER,8,workspace,8
-      bind=SUPER,9,workspace,9
-      bind=SUPER,0,workspace,10
-
-      bind=SUPER_SHIFT,1,movetoworkspace,1
-      bind=SUPER_SHIFT,2,movetoworkspace,2
-      bind=SUPER_SHIFT,3,movetoworkspace,3
-      bind=SUPER_SHIFT,4,movetoworkspace,4
-      bind=SUPER_SHIFT,5,movetoworkspace,5
-      bind=SUPER_SHIFT,6,movetoworkspace,6
-      bind=SUPER_SHIFT,7,movetoworkspace,7
-      bind=SUPER_SHIFT,8,movetoworkspace,8
-      bind=SUPER_SHIFT,9,movetoworkspace,9
-      bind=SUPER_SHIFT,0,movetoworkspace,10
-
-      bind=SUPER,mouse_down,workspace,e+1
-      bind=SUPER,mouse_up,workspace,e-1
-    '';
+    extraConfig = pkgs.lib.strings.concatStringsSep "\n" [
+      monitors
+      binds
+      animations
+      execs
+    ];
   };
+
+  home.file.".config/hypr/hyprpaper.conf".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/hypr/hyprpaper.conf";
 
 }
