263d7b3ad791b06af2bf2d10676165dfd8324522
shebpamm <erik@opcode.xyz>
2022-11-29 16:00:20 +0200

    add greetd for starting hypr

--- modules/system/graphics/wayland.nix
+++ modules/system/graphics/wayland.nix
@@ -7,5 +7,16 @@
     package = null;
   };
 
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "Hyprland";
+        user = "shebpamm";
+      };
+      default_session = initial_session;
+    };
+  };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 }
