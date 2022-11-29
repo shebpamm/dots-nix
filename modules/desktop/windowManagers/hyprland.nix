654cde381c063428736dd5054f219e15cdbed729
shebpamm <erik@opcode.xyz>
2022-11-29 15:23:17 +0200

    improve hyprland config

--- modules/desktop/windowManagers/hyprland.nix
+++ modules/desktop/windowManagers/hyprland.nix
@@ -1,20 +1,29 @@
 { config, pkgs, libs, inputs, ... }:
 let
   configPath = ../../../config/hypr;
-  monitors = (builtins.readFile (configPath + /monitors.conf));
-  binds = (builtins.readFile (configPath + /binds.conf));
+
   animations = (builtins.readFile (configPath + /animations.conf));
+  binds = (builtins.readFile (configPath + /binds.conf));
+  colors = (builtins.readFile (configPath + /colors/latte.conf));
+  decoration = (builtins.readFile (configPath + /decoration.conf));
   execs = (builtins.readFile (configPath + /execs.conf));
+  general = (builtins.readFile (configPath + /general.conf));
+  monitors = (builtins.readFile (configPath + /monitors.conf));
+  rules = (builtins.readFile (configPath + /rules.conf));
 in
 {
   imports = [ inputs.hyprland.homeManagerModules.default ];
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = pkgs.lib.strings.concatStringsSep "\n" [
-      monitors
-      binds
       animations
+      binds
+      colors
+      decoration
       execs
+      general
+      monitors
+      rules
     ];
   };
 
