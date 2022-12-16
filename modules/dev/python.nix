31d93abad9362a787f0f02b9c78367cb72a4f039
shebpamm <erik@opcode.xyz>
2022-12-16 17:21:31 +0200

    lint python

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -1,12 +1,13 @@
 { config, pkgs, libs, ... }:
-  let
-    pythonEnv = (pkgs.python39.withPackages (ps: [
-      ps.pip
-      ps.openai
-    ])).override (args: { ignoreCollisions = true; });
-  in {
-    home.packages = with pkgs; [
-      pythonEnv
-      poetry
-    ];
-  }
+let
+  pythonEnv = (pkgs.python39.withPackages (ps: [
+    ps.pip
+    ps.openai
+  ])).override (args: { ignoreCollisions = true; });
+in
+{
+  home.packages = with pkgs; [
+    pythonEnv
+    poetry
+  ];
+}
