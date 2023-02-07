7bb6038ce4d7ab8e4da9bd893dc3e24de75074cf
shebpamm <erik@opcode.xyz>
2023-02-07 17:32:25 +0200

    update spicetify config

--- modules/programs/spotify.nix
+++ modules/programs/spotify.nix
@@ -1,13 +1,17 @@
 { config, pkgs, libs, inputs, ... }:
 {
   imports = [ inputs.spicetify.homeManagerModule ];
-  programs.spicetify = {
-    enable = true;
-    theme = "catppuccin-latte";
-    enabledExtensions = [
-      "fullAppDisplay.js"
-      "shuffle+.js"
-      "hidePodcasts.js"
-    ];
-  };
+  programs.spicetify =
+    let
+      spicePkgs = inputs.spicetify.packages.${pkgs.system}.default;
+    in
+    {
+      enable = true;
+      theme = spicePkgs.themes.catppuccin-latte;
+      enabledExtensions = with spicePkgs.extensions; [
+        fullAppDisplay
+        shuffle
+        hidePodcasts
+      ];
+    };
 }
