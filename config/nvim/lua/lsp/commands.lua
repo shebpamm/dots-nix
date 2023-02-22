c6cea9288ae2f865a89a48b820f0a9fc7d999a03
shebpamm <erik@opcode.xyz>
2023-02-22 10:59:00 +0200

    remove broken command causing headache

--- config/nvim/lua/lsp/commands.lua
+++ config/nvim/lua/lsp/commands.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 function M.setup()
-  utils.command("LspFormat", vim.lsp.buf.format { timeout_ms = 5000 })
   utils.command("ToggleFormatOnSave", require("lsp.utils").toggle_format_on_save)
   utils.command("LspLog", "edit " .. vim.lsp.get_log_path())
 end
