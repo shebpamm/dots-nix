451351580533936cf0af58df1c4a68f10b093cb8
shebpamm <erik@opcode.xyz>
2023-02-25 16:30:15 +0200

    completely rehaul null-ls setup to allow overriding settings from vim.g.NULL_LS_OVERRIDES

--- config/nvim/lua/lsp/null-ls.lua
+++ config/nvim/lua/lsp/null-ls.lua
@@ -2,58 +2,74 @@ local M = {}
 
 require("mason-null-ls").setup { automatic_installation = true }
 
+local null_ls = require "null-ls"
+local b = null_ls.builtins
+
+local sources = {
+  b.code_actions.refactoring,
+  b.diagnostics.flake8,
+  b.diagnostics.pydocstyle,
+  b.diagnostics.shellcheck,
+  b.diagnostics.sqlfluff,
+  b.diagnostics.tfsec,
+  b.diagnostics.yamllint,
+  b.formatting.astyle,
+  b.formatting.black,
+  b.formatting.isort,
+  b.formatting.prettierd,
+  b.formatting.sqlfluff,
+  b.formatting.stylua,
+  b.formatting.terraform_fmt,
+  b.formatting.yamlfmt,
+}
+
+local default_overrides = {
+  flake8 = {
+    filter = function(diagnostic)
+      return diagnostic.code ~= "E501" -- Skip "line too long" errors
+    end,
+  },
+  yamllint = {
+    filter = function(d)
+      if d.code == "document-start" then return false end
+      if d.code == "line-length" then return false end
+
+      return true
+    end,
+  },
+  astyle = {
+    extra_filetypes = { "groovy", "Jenkinsfile" },
+  },
+  sqlfluff = {
+    extra_args = { "--dialect", "postgres" }, -- change to your dialect
+  },
+}
+
+local function configure_sources(srcs, defaults)
+  local configured_sources = {}
+  local global_overrides = vim.g.null_ls_overrides or {}
+  local overrides = vim.tbl_extend("force", defaults, global_overrides)
+  for _, src in pairs(srcs) do
+    local override = overrides[src.name]
+    if override == nil then
+      table.insert(configured_sources, src)
+    else
+      table.insert(
+        configured_sources,
+        src.with(override)
+      )
+    end
+  end
+
+  return configured_sources
+end
+
 M.setup = function()
-  local null_ls = require "null-ls"
-  local b = null_ls.builtins
 
   null_ls.setup {
     debounce = 150,
     debug = true,
-    sources = {
-      b.diagnostics.flake8.with {
-        filter = function(diagnostic)
-          return diagnostic.code ~= "E501" -- Skip "line too long" errors
-        end,
-      },
-      b.diagnostics.yamllint.with {
-        filter = function(d)
-          if d.code == "document-start" then return false end
-          if d.code == "line-length" then return false end
-
-          return true
-        end,
-      },
-      b.formatting.astyle.with {
-        extra_filetypes = { "groovy", "Jenkinsfile" },
-      },
-      b.diagnostics.sqlfluff.with {
-        extra_args = { "--dialect", "postgres" }, -- change to your dialect
-      },
-      b.formatting.sqlfluff.with {
-        extra_args = { "--dialect", "postgres" }, -- change to your dialect
-      },
-      b.code_actions.refactoring,
-      b.diagnostics.pydocstyle,
-      b.diagnostics.shellcheck,
-      b.diagnostics.tfsec,
-      b.formatting.black,
-      b.formatting.isort,
-      b.formatting.prettierd,
-      b.formatting.stylua,
-      b.formatting.terraform_fmt,
-      b.formatting.yamlfmt,
-    },
-    -- on_attach = function(client)
-    --   if client.server_capabilities.document_formatting then
-    --     utils.augroup("lsp_format", {
-    --       {
-    --         events = { "BufWritePre" },
-    --         targets = { "<buffer>" },
-    --         command = require("lsp.utils").format_on_save,
-    --       },
-    --     })
-    --   end
-    -- end,
+    sources = configure_sources(sources, default_overrides),
   }
 end
 
