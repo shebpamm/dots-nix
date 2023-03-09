14eae32ab062e3551630f377e638bb4f008473a6
shebpamm <erik@opcode.xyz>
2023-03-09 14:29:17 +0200

    remove python globals

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -1,13 +1,6 @@
 { config, pkgs, libs, ... }:
-let
-  pythonEnv = (pkgs.python39.withPackages (ps: [
-    ps.pip
-    ps.openai
-  ])).override (args: { ignoreCollisions = true; });
-in
 {
   home.packages = with pkgs; [
-    pythonEnv
-    poetry
+    python39
   ];
 }
