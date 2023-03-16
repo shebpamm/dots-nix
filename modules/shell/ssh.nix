7edfcd36c4cb46fa2b8db0de8fbfed92bfbdca14
shebpamm <erik@opcode.xyz>
2023-03-16 23:11:41 +0200

    ssh: don't use extraConfig, use real properties

--- modules/shell/ssh.nix
+++ modules/shell/ssh.nix
@@ -1,38 +1,39 @@
 { config, pkgs, libs, ... }:
 {
   programs.ssh = {
-      enable = true;
-      forwardAgent = true;
-      hashKnownHosts = true;
+    enable = true;
+    forwardAgent = true;
+    hashKnownHosts = true;
 
-      matchBlocks = {
-          "*" = {
-              user = "shebpamm";
-              extraOptions = {
-                SetEnv = "TERM=xterm";
-                ControlMaster = "auto";
-                ControlPath = "/run/user/%i/master-%l-%r@%h:%p";
-                ControlPersist = "5m";
-              };
-          };
-          showrunner = {
-              user = "shebpamm";
-          };
-          "askola-ssh.opcode.xyz" = {
-            user = "shebpamm123";
-            extraOptions = {
-              ProxyCommand = "bash -c '/home/shebpamm/.nix-profile/bin/cloudflared access ssh-gen --hostname %h; ssh -tt shebpamm123@cfpipe-askola-ssh.opcode.xyz >&2 <&1'";
-            };
-          };
+    controlMaster = "auto";
+    controlPath = "/run/user/%i/master-%l-%r@%h:%p";
+    controlPersist = "15m";
 
-          "cfpipe-askola-ssh.opcode.xyz" = {
-            extraOptions = {
-              HostName = "askola-ssh.opcode.xyz";
-              ProxyCommand = "/home/shebpamm/.nix-profile/bin/cloudflared access ssh --hostname %h";
-              IdentityFile = "~/.cloudflared/askola-ssh.opcode.xyz-cf_key";
-              CertificateFile = "~/.cloudflared/askola-ssh.opcode.xyz-cf_key-cert.pub";
-            };
-          };
+    matchBlocks = {
+      "*" = {
+        user = "shebpamm";
+        extraOptions = {
+          SetEnv = "TERM=xterm";
         };
+      };
+      showrunner = {
+        user = "shebpamm";
+      };
+      "askola-ssh.opcode.xyz" = {
+        user = "shebpamm123";
+        extraOptions = {
+          ProxyCommand = "bash -c '/home/shebpamm/.nix-profile/bin/cloudflared access ssh-gen --hostname %h; ssh -tt shebpamm123@cfpipe-askola-ssh.opcode.xyz >&2 <&1'";
+        };
+      };
+
+      "cfpipe-askola-ssh.opcode.xyz" = {
+        extraOptions = {
+          HostName = "askola-ssh.opcode.xyz";
+          ProxyCommand = "/home/shebpamm/.nix-profile/bin/cloudflared access ssh --hostname %h";
+          IdentityFile = "~/.cloudflared/askola-ssh.opcode.xyz-cf_key";
+          CertificateFile = "~/.cloudflared/askola-ssh.opcode.xyz-cf_key-cert.pub";
+        };
+      };
     };
+  };
 }
