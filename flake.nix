141dcd75c02f52c9f76700022ef92258bc81786d
shebpamm <erik@opcode.xyz>
2023-04-05 15:33:49 +0300

    hexane: tweak ssh settings

--- flake.nix
+++ flake.nix
@@ -116,9 +116,10 @@
               programs.home-manager.enable = true;
               
               programs.ssh.matchBlocks."*".extraOptions = { IdentityAgent = "~/.1password/agent.sock"; };
-              programs.ssh.matchBlocks."*.smartly.io".user = "erik";
+              programs.ssh.matchBlocks."*".user = "erik";
+              programs.ssh.includes = [ "~/.ssh/config.work" ];
 
-              home.sessionVariables.SSH_AUTH_SOCK = "{home.homeDirectory}/.1password/agent.sock";
+              home.sessionVariables.SSH_AUTH_SOCK = "${home.homeDirectory}/.1password/agent.sock";
             }
             ./hm/desktop
             ./hm/desktop/windowManagers/awesome.nix
