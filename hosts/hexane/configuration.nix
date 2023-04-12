a3dccd18f1556bbea75a246b7739e995ab767e6a
shebpamm <erik@opcode.xyz>
2023-04-12 10:23:32 +0300

    hexane: backup home

--- hosts/hexane/configuration.nix
+++ hosts/hexane/configuration.nix
@@ -91,6 +91,22 @@
       options = [ "subvol=@nix" "compress=zstd" "noatime" ];
     };
 
+  services.btrbk.instances.btrbk.onCalendar = "daily";
+  services.btrbk.instances.btrbk.settings = {
+    snapshot_preserve = "14d";
+    snapshot_preserve_min = "2d";
+    volume = {
+      "/" = {
+        subvolume = {
+          home = {
+            snapshot_create = "always";
+          };
+        };
+        target = "/snapshots";
+      };
+    };
+  };
+
   services.sshd.enable = true;
 
   system.stateVersion = "21.11";
