9556d13c20e47859dbd32a8e5a7106c919ce09d1
Erik Karsten <erik@opcode.xyz>
2023-04-24 16:55:14 +0300

    world: tighten gotmpl detection in nvim

--- config/nvim/lua/detect.lua
+++ config/nvim/lua/detect.lua
@@ -1,9 +1,13 @@
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = {"*"},
+  pattern = { "*" },
   callback = function()
     local header = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
     if header == "---" then
-      vim.bo.filetype = "gotmpl"
+      if vim.bo.filetype ~= "yaml.ansible" then
+        if vim.fn.search('{{.\\+}}', 'nw') ~= 0 then
+          vim.bo.filetype = "gotmpl"
+        end
+      end
     end
   end,
 })
