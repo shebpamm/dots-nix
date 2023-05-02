61052c38e475dde2630de12d13e6ff80345c4806
Erik Karsten <erik@opcode.xyz>
2023-05-02 15:06:55 +0300

    world: add spotify-player

--- hm/programs/spotify.nix
+++ hm/programs/spotify.nix
@@ -1,6 +1,9 @@
 { config, pkgs, libs, inputs, ... }:
 {
   imports = [ inputs.spicetify.homeManagerModule ];
+  home.packages = with pkgs; [
+    spotify-player
+  ];
   programs.spicetify =
     let
       spicePkgs = inputs.spicetify.packages.${pkgs.system}.default;
