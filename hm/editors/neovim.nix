a903e9737ff06401f9dcc8717fe853a6972d6c6f
Erik Karsten <erik@opcode.xyz>
2023-06-06 22:21:11 +0300

    add lsp dependencies to nvim

--- hm/editors/neovim.nix
+++ hm/editors/neovim.nix
@@ -4,8 +4,33 @@
     enable = true;
     package = pkgs.neovim-nightly;
     defaultEditor = true;
+    withPython3 = true;
+    withNodeJs = true;
+    withRuby = true;
     extraPackages = with pkgs; [
-
+      nodePackages.prettier_d_slim
+      nodePackages.typescript-language-server
+      nodePackages.dockerfile-language-server-nodejs
+      nodePackages.vscode-css-languageserver-bin
+      shellcheck
+      sqlfluff
+      stylua
+      tfsec
+      yamlfmt
+      yamllint
+      gopls
+      tflint
+      terraform-ls
+      taplo
+      sqls
+      dot-language-server
+      ansible-language-server
+    ];
+    extraPython3Packages = ppkgs: with ppkgs; [
+      black
+      isort
+      flake8
+      pydocstyle
     ];
   };
   home.file.".config/nvim".source =
