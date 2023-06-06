a9e0a27cca91d0763ecbbf7a635bafe79ab80c25
Erik Karsten <erik@opcode.xyz>
2023-06-06 23:44:37 +0300

    nvim: add nui to core.lua

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -2,4 +2,5 @@ return {
   "nvim-lua/plenary.nvim", -- Lua helpers
   "kyazdani42/nvim-web-devicons", -- Icons
   "stevearc/dressing.nvim", -- Nicer UI elements
+  "MunifTanjim/nui.nvim", -- Widget Libary
 }
