e1932be50b27133b83b68c061d0d513b3ec06760
Erik Karsten <erik@opcode.xyz>
2023-06-07 16:03:01 +0300

    nvim: lazyload comment.lua

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,22 @@
 return {
   "numToStr/Comment.nvim",
+  keys = {
+    "gc",
+    "gb",
+    "gcc",
+    "gbc",
+    "gbc",
+    { "gc", mode = "v" },
+    { "gb", mode = "v" }
+  },
   config = function()
-    require("Comment").setup()
+    require("Comment").setup {
+      padding = true,
+      sticky = true,
+      mappings = {
+        basic = true,
+        extra = true,
+      },
+    }
   end,
 }
