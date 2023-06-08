abfa95394dba499f66deb2db9301a622919ccedc
Erik Karsten <erik@opcode.xyz>
2023-06-08 09:06:14 +0300

    nvim: lazyload grapple

--- config/nvim/lua/plugins/grapple.lua
+++ config/nvim/lua/plugins/grapple.lua
@@ -1,9 +1,47 @@
+local function qf()
+  if utils.is_open "c" then
+    vim.cmd "cclose"
+  else
+    require("grapple").quickfix()
+  end
+end
+
 return {
   "cbochs/grapple.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>rq", qf, desc = "grapple quickfix" },
+    {
+      "<leader>rt",
+      function()
+        require("grapple").popup_tags()
+      end,
+      desc = "show grapple tags",
+    },
+    {
+      "<leader>rs",
+      function()
+        require("grapple").popup_scopes()
+      end,
+      desc = "show grapple scopes",
+    },
+    {
+      "<leader>rd",
+      function()
+        require("grapple").untag()
+      end,
+      desc = "grapple untag",
+    },
+    {
+      "<leader>ra",
+      function()
+        require("grapple").tag()
+      end,
+      desc = "grapple tag",
+    },
+  },
   config = function()
     local gr = require "grapple"
-    local wk = require "which-key"
 
     gr.setup {
       log_level = "warn",
@@ -15,27 +53,6 @@ return {
         style = "minimal",
         focusable = false,
         border = "single",
-      },
-    }
-
-    local function qf()
-      if utils.is_open "c" then
-        vim.cmd "cclose"
-      else
-        gr.quickfix()
-      end
-    end
-
-    wk.register {
-      ["<leader>"] = {
-        r = {
-          name = "+grapple",
-          q = { qf, "quickfix" },
-          t = { gr.popup_tags, "show tags" },
-          s = { gr.popup_scopes, "show scopes" },
-          d = { gr.untag, "untag" },
-          a = { gr.tag, "tag" },
-        },
       },
     }
   end,
