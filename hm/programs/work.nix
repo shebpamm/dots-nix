09e06dafb77c848d29762747e9d274e70f4a1fff
Erik Karsten <erik@opcode.xyz>
2023-06-08 14:14:25 +0300

    hexane: new work package go-jira

--- hm/programs/work.nix
+++ hm/programs/work.nix
@@ -8,6 +8,7 @@
     vault
     git-crypt
     aws-vault
+    go-jira
   ];
 
   home.file.".config/keepassxc/keepassxc.ini".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/keepassxc/keepassxc.ini";
