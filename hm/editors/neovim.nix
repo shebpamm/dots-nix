7e3350770b024e0ab369eef646d4e95422aebc8d
Erik Karsten <erik@opcode.xyz>
2023-06-15 11:39:04 +0300

    world: add pyright to nvim packages

--- hm/editors/neovim.nix
+++ hm/editors/neovim.nix
@@ -6,6 +6,7 @@ let
     nodePackages.typescript-language-server
     nodePackages.dockerfile-language-server-nodejs
     nodePackages.vscode-css-languageserver-bin
+    nodePackages.pyright
     shellcheck
     sqlfluff
     stylua
