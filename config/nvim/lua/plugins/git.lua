892225afc9902e27aceb6bcefb85acf9468a3082
Erik Karsten <erik@opcode.xyz>
2023-07-14 17:09:31 +0300

    nvim: update plugins, fix keymappings in gitsigns

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -3,6 +3,17 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "BufRead",
     module = "gitsigns",
+    keys = {
+      { "<leader>vs",	function() require"gitsigns".stage_hunk() end,	                                        mode = "n" },
+      { "<leader>vs",	function() require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")}) end,      mode = "v" },
+      { "<leader>vu",	function() require"gitsigns".undo_stage_hunk() end,	                                mode = "n" },
+      { "<leader>vr",	function() require"gitsigns".reset_hunk() end,	                                        mode = "n" },
+      { "<leader>vr",	function() require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")}) end,	mode = "v" },
+      { "<leader>vR",	function() require"gitsigns".reset_buffer() end,	                                mode = "n" },
+      { "<leader>vp",	function() require"gitsigns".preview_hunk() end,	                                mode = "n" },
+      { "<leader>vb",	function() require"gitsigns".toggle_current_line_blame() end,	                        mode = "n" },
+
+    },
     config = function()
       require("gitsigns").setup {
         signs = {
@@ -11,33 +22,6 @@ return {
           delete = { text = "-" },
           topdelete = { text = "-" },
           changedelete = { text = "~" },
-        },
-        keymaps = {
-          -- Default keymap options
-          noremap = true,
-          buffer = true,
-
-          ["n ]c"] = {
-            expr = true,
-            "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'",
-          },
-          ["n [c"] = {
-            expr = true,
-            "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'",
-          },
-
-          ["n <leader>vs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-          ["v <leader>vs"] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-          ["n <leader>vu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-          ["n <leader>vr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-          ["v <leader>vr"] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-          ["n <leader>vR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-          ["n <leader>vp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-          ["n <leader>vb"] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
-
-          -- Text objects
-          ["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-          ["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
         },
       }
     end,
