c63c7ac486f1336ada7cc7ac4665c3efdd96bf45
Erik Karsten <erik@opcode.xyz>
2023-07-14 16:00:42 +0300

    nvim: lsp goto mappings broke, move them to lazy for now

--- config/nvim/lua/plugins/goto.lua
+++ config/nvim/lua/plugins/goto.lua
@@ -1,10 +1,15 @@
 return {
   "rmagatti/goto-preview",
   keys = {
-        { "gpd", function() require("goto-preview").goto_preview_definition() end, desc = "preview definition" },
-        { "gpD", function() require("goto-preview").goto_preview_type_definition() end, desc = "preview type definition" },
-        { "gpi", function() require("goto-preview").goto_preview_implementation() end, desc = "preview implementation" },
-        { "gpr", function() require("goto-preview").goto_preview_references() end, desc = "preview references" },
+    { "gpd", function() require("goto-preview").goto_preview_definition() end,      desc = "preview definition" },
+    { "gpD", function() require("goto-preview").goto_preview_type_definition() end, desc = "preview type definition" },
+    { "gpi", function() require("goto-preview").goto_preview_implementation() end,  desc = "preview implementation" },
+    { "gpr", function() require("goto-preview").goto_preview_references() end,      desc = "preview references" },
+    { "gd",  vim.lsp.buf.definition,                                                desc = "definition" },
+    { "gD",  vim.lsp.buf.type_definition,                                           desc = "type definition" },
+    { "gi",  vim.lsp.buf.implementation,                                            desc = "implementation" },
+    { "gr",  vim.lsp.buf.references,                                                desc = "references" },
+
   },
   config = function()
     local gp = require "goto-preview"
