51d58b41ded025dabd5f9143084055941a6bfbcb
Erik Karsten <erik@opcode.xyz>
2023-07-18 23:47:45 +0300

    kerosene: use os/system/default.nix

--- hosts/kerosene/configuration.nix
+++ hosts/kerosene/configuration.nix
@@ -7,16 +7,13 @@
       ./hardware-configuration.nix
 
       # Other Modules
-      ../../os/system/env.nix
-      ../../os/system/fonts.nix
-      ../../os/system/sound.nix
-      ../../os/system/users.nix
+      ../../os/system
       # ../../os/system/graphics/wayland.nix
-      ../../os/system/graphics/xorg.nix
       ../../os/games/steam.nix
       ../../os/secrets
-      ../../os/virtualization/lxd.nix
+      ../../os/system/graphics/xorg.nix
       ../../os/virtualization/kvm.nix
+      ../../os/virtualization/lxd.nix
     ];
 
   nixpkgs.config.allowUnfree = true;
@@ -110,33 +107,6 @@
     zsh
   ];
 
-
-  nix = {
-    package = pkgs.nixUnstable;
-    settings = {
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
-        "https://devenv.cachix.org"
-        "https://domenkozar.cachix.org"
-        "https://fortuneteller2k.cachix.org"
-        "https://hyprland.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "domenkozar.cachix.org-1:Qf0xcQX3VUvo5/c6xTvbAET3EEtq1E2Mk1vojgwBprE="
-        "fortuneteller2k.cachix.org-1:kXXNkMV5yheEQwT0I4XYh1MaCSz+qg72k8XAi2PthJI="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
-    };
-
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
 
   virtualisation.docker.enable = true;
   virtualisation.docker.enableNvidia = true;
