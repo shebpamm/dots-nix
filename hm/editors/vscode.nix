d873b39a5160f2208ac8b563c709c0f52a4f9e8a
Erik Karsten <erik@opcode.xyz>
2023-07-19 14:28:49 +0300

    world: nixify vscode

--- hm/editors/vscode.nix
+++ hm/editors/vscode.nix
@@ -1,4 +1,21 @@
-{ config, pkgs, libs, ... }:
+{ pkgs, config, ... }:
 {
-  home.packages = [ pkgs.vscode-insider ];
+  programs.vscode = {
+    enable = true;
+    enableExtensionUpdateCheck = false;
+    enableUpdateCheck = false;
+    package = pkgs.vscode-insider;
+    extensions = with pkgs.vscode-extensions; [
+      github.copilot
+      github.copilot-chat
+      asvetliakov.vscode-neovim
+    ];
+    userSettings = {
+      "extensions.experimental.affinity"."asvetliakov.vscode-neovim" = 1;
+    };
+  };
+  home.file.".config/Code - Insiders".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/Code";
+  home.file.".vscode-insiders.source" = 
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.vscode";
 }
