4eff3b56125682534f7c0d027635fb3b0085c284
Erik Karsten <erik@opcode.xyz>
2023-07-19 15:19:05 +0300

    world: add some vscode extensions

--- hm/editors/vscode.nix
+++ hm/editors/vscode.nix
@@ -8,15 +8,22 @@
     extensions = with pkgs.vscode-extensions; [
       github.copilot
       github.copilot-chat
+
       asvetliakov.vscode-neovim
+
+      catppuccin.catppuccin-vsc
+      catppuccin.catppuccin-vsc-icons
+
+      bbenoist.nix
     ];
     userSettings = {
       "extensions.experimental.affinity"."asvetliakov.vscode-neovim" = 1;
       "keyboard.dispatch" = "keyCode";
+      "workbench.colorTheme" = "Catppuccin Mocha";
     };
   };
   home.file.".config/Code - Insiders".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/Code";
-  home.file.".vscode-insiders".source = 
+  home.file.".vscode-insiders".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.vscode";
 }
