6caa028da62bc57428f3a83597651ae4fabe7127
Erik Karsten <erik@opcode.xyz>
2023-07-21 15:04:28 +0300

    world: change to fork of git-crypt

--- hm/programs/work.nix
+++ hm/programs/work.nix
@@ -1,4 +1,18 @@
 { config, pkgs, libs, ... }:
+let
+  git-crypt-worktrees = pkgs.git-crypt.overrideAttrs (oldAttrs: rec {
+    pname = "git-crypt";
+    version = "0.6.0";
+    src = pkgs.fetchFromGitHub
+      {
+        owner = "morganwahl";
+        repo = pname;
+        rev = "use-common-git-dir";
+        sha256 = "sha256-f4FjLfVmfLcHpLITQCQaobU10aHifPqhCpamjUrMI/A";
+      };
+  });
+
+in
 {
   home.packages = with pkgs; [
     slack
@@ -6,7 +20,7 @@
     openvpn
     libsecret
     vault
-    git-crypt
+    git-crypt-worktrees
     aws-vault
     go-jira
   ];
