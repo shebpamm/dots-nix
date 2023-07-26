f90d6796e5692a6671835e88b812bb6c36c9c157
Erik Karsten <erik@opcode.xyz>
2023-07-26 10:49:20 +0300

    nvim: don't save sessions with only a single buffer open

--- config/nvim/lua/plugins/sessions.lua
+++ config/nvim/lua/plugins/sessions.lua
@@ -1,27 +1,22 @@
 return {
-  "Shatur/neovim-session-manager",
+  "shebpamm/neovim-session-manager", 
   event = "VimEnter",
   config = function()
     local s = require "session_manager"
     local sc = require "session_manager.config"
-    local wk = require "which-key"
 
     s.setup {
       autoload_mode = sc.AutoloadMode.Disabled,
-    }
-
-    wk.register {
-      ["<leader>"] = {
-        -- find
-        s = {
-          name = "+session",
-          f = { "<cmd>SessionManager load_session<cr>", "Load Session" },
-          l = { "<cmd>SessionManager load_last_session<cr>", "Last Session" },
-          c = { "<cmd>SessionManager load_current_dir_session<cr>", "Current Directory Session" },
-          s = { "<cmd>SessionManager save_current_session<cr>", "Save Session" },
-          d = { "<cmd>SessionManager delete_session<cr>", "Delete Session" },
-        },
-      },
+      autosave_extra_predicate = function()
+        return #vim.fn.getbufinfo { buflisted = 1 } > 1
+      end,
     }
   end,
+  keys = {
+    { "<leader>sf", "<cmd>SessionManager load_session<cr>", desc = "Load Session" },
+    { "<leader>sl", "<cmd>SessionManager load_last_session<cr>", desc = "Last Session" },
+    { "<leader>sc", "<cmd>SessionManager load_current_dir_session<cr>", desc = "Current Directory Session" },
+    { "<leader>ss", "<cmd>SessionManager save_current_session<cr>", desc = "Save Session" },
+    { "<leader>sd", "<cmd>SessionManager delete_session<cr>", desc = "Delete Session" },
+  },
 }
