d79798539a3d20dcef8b8be0042f094b501cf5da
Erik Karsten <erik@opcode.xyz>
2023-07-26 17:44:08 +0300

    nvim: make delaytrain toggleable

--- config/nvim/lua/plugins/delaytrain.lua
+++ config/nvim/lua/plugins/delaytrain.lua
@@ -1,9 +1,18 @@
 return {
   "ja-ford/delaytrain.nvim",
   init = function()
-    require('delaytrain').setup {
+    require("delaytrain").setup {
       delay_ms = 800,
       grace_period = 1,
     }
   end,
+  keys = {
+    {
+      "<leader>td",
+      function()
+        require("delaytrain").toggle()
+      end,
+      desc = "training delay",
+    },
+  },
 }
