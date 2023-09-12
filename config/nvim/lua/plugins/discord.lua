2ab5dc0c88d24f5fbb4962a54e4ac1bf0f4bebf8
Erik Karsten <erik@opcode.xyz>
2023-09-12 23:31:41 +0300

    nvim: adjust logging

--- config/nvim/lua/plugins/discord.lua
+++ config/nvim/lua/plugins/discord.lua
@@ -5,7 +5,7 @@ return {
     auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
     neovim_image_text   = "yeah...", -- Text displayed when hovered over the Neovim image
     main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
-    log_level           = "debug",                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
     debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
     enable_line_number  = true,                      -- Displays the current line number instead of the current project
     blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
