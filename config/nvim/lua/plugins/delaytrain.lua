057cc1a60808db3f23abb0b987a62cded708e70a
Erik Karsten <erik@opcode.xyz>
2023-10-18 14:36:33 +0200

    nvim: remove delaytrain

--- config/nvim/lua/plugins/delaytrain.lua
+++ config/nvim/lua/plugins/delaytrain.lua
@@ -1,18 +1,18 @@
 return {
-  "ja-ford/delaytrain.nvim",
-  init = function()
-    require("delaytrain").setup {
-      delay_ms = 800,
-      grace_period = 2,
-    }
-  end,
-  keys = {
-    {
-      "<leader>td",
-      function()
-        require("delaytrain").toggle()
-      end,
-      desc = "training delay",
-    },
-  },
+  -- "ja-ford/delaytrain.nvim",
+  -- init = function()
+  --   require("delaytrain").setup {
+  --     delay_ms = 800,
+  --     grace_period = 2,
+  --   }
+  -- end,
+  -- keys = {
+  --   {
+  --     "<leader>td",
+  --     function()
+  --       require("delaytrain").toggle()
+  --     end,
+  --     desc = "training delay",
+  --   },
+  -- },
 }
