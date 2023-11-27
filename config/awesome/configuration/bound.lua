e68b2fac2be4a2cd2fe782ac11570a4bd8fe42c1
Erik Karsten <erik@opcode.xyz>
2023-11-27 21:25:48 +0200

    awm: remove duplicate keys

--- config/awesome/configuration/bound.lua
+++ config/awesome/configuration/bound.lua
@@ -150,18 +150,18 @@ awful.keyboard.append_global_keybindings {
 
 -- Layout related keybindings
 awful.keyboard.append_global_keybindings {
-  awful.key({ modkey, "Shift" }, "j", function()
-    awful.client.swap.byidx(1)
-  end, {
-    description = "swap with next client by index",
-    group = "client",
-  }),
-  awful.key({ modkey, "Shift" }, "k", function()
-    awful.client.swap.byidx(-1)
-  end, {
-    description = "swap with previous client by index",
-    group = "client",
-  }),
+--   awful.key({ modkey, "Shift" }, "j", function()
+--     awful.client.swap.byidx(1)
+--   end, {
+--     description = "swap with next client by index",
+--     group = "client",
+--   }),
+--   awful.key({ modkey, "Shift" }, "k", function()
+--     awful.client.swap.byidx(-1)
+--   end, {
+--     description = "swap with previous client by index",
+--     group = "client",
+--   }),
   awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
   awful.key({ modkey }, "l", function()
     awful.tag.incmwfact(0.05)
