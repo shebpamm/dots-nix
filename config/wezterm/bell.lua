021ad5617785d16d0b9047329d9fb39186617036
Erik Karsten <erik@opcode.xyz>
2023-12-13 09:32:27 +0200

    wez: disable bell

---
+++ config/wezterm/bell.lua
@@ -0,0 +1,3 @@
+return {
+  audible_bell = "Disabled"
+}
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local config = {}
 
 config = tbl.merge(config, require 'themes.catppuccin')
 config = tbl.merge(config, require 'keyboard')
+config = tbl.merge(config, require 'bell')
 
 config.scrollback_lines = 3500
 
