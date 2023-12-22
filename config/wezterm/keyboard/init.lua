a3622d9f85aed8848e890f9e0ac1cdabe358d7ba
Erik Karsten <erik@opcode.xyz>
2023-12-22 12:22:44 +0200

    wez: rename tabs

--- config/wezterm/keyboard/init.lua
+++ config/wezterm/keyboard/init.lua
@@ -5,5 +5,6 @@ local config = {}
 
 config = tbl.merge(config, require 'keyboard.tabs')
 config = tbl.merge(config, require 'keyboard.select')
+config = tbl.merge(config, require 'keyboard.rename')
 
 return config
---
+++ config/wezterm/keyboard/rename.lua
@@ -0,0 +1,23 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+return {
+	keys = {
+		{
+			key = "R",
+			mods = "CTRL|SHIFT",
+			action = act.PromptInputLine({
+				description = "Enter new name for tab",
+				action = wezterm.action_callback(function(window, _, line)
+					-- line will be `nil` if they hit escape without entering anything
+					-- An empty string if they just hit enter
+					-- Or the actual line of text they wrote
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
+		-- other keys
+	},
+}
