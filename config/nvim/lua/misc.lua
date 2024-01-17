f5fa359c969150f4da4c7cc8108cecf526649e74
Erik Karsten <erik@opcode.xyz>
2024-01-17 08:41:51 +0200

    nvim: alt-v to open vscode

--- config/nvim/lua/misc.lua
+++ config/nvim/lua/misc.lua
@@ -7,6 +7,18 @@ utils.augroup("tmux-cursor", {
   },
 })
 
+-- Open VS Code at the cursor
+local function open_vscode()
+  local line = vim.fn.line(".")
+  local column = vim.fn.col(".")
+  local file = vim.fn.expand("%:p")
+  local command = string.format("code --goto '%s:%s:%s'", file, line, column)
+  vim.fn.system(command)
+end
+
+utils.command("VSCode", open_vscode)
+utils.nnoremap("<M-v>", ":VSCode<CR>")
+
 -- Center screen after up-down movement
 utils.nnoremap("<C-d>", "<C-d>zz")
 utils.nnoremap("<C-u>", "<C-u>zz")
