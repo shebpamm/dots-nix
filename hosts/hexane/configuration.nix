aa487062a675334a831b905eca3ff7e9e4c42d04
Erik Karsten <erik@opcode.xyz>
2024-07-25 11:15:11 +0300

    hexane: disable fingerprint

--- hosts/hexane/configuration.nix
+++ hosts/hexane/configuration.nix
@@ -11,7 +11,7 @@
       # Other Modules
       ../../os/system
       ../../os/system/graphics/xorg.nix
-      ../../os/system/fingerprint.nix
+      # ../../os/system/fingerprint.nix
       ../../os/secrets
      ../../os/system/keyboard.nix
 
