7d09a4a25dd2447bad7e9ec7d13f289387ba4849
Erik Karsten <erik@opcode.xyz>
2024-08-22 11:12:49 +0300

    nvim: fix bindings for marks

--- config/nvim/lua/plugins/marks.lua
+++ config/nvim/lua/plugins/marks.lua
@@ -1,24 +1,16 @@
 return {
   {
     "chentoast/marks.nvim",
+    lazy = false,
     opts = {
       default_mappings = true,
       builtin_marks = { ".", "^", "<", ">" },
       cyclic = true,
     },
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      ["<leader>"] = {
-        -- find
-        m = {
-          name = "+marks",
-          t = { "<cmd>MarksToggleSigns<cr>", "toggle signs" },
-          l = { "<cmd>MarksQFListAll<cr>", "list all marks" },
-          b = { "<cmd>BookmarksQFListAll<cr>", "list all bookmarks" },
-        },
-      },
+    keys = {
+      { "<leader>mt", "<cmd>MarksToggleSigns<cr>", desc = "toggle signs" },
+      { "<leader>ml", "<cmd>MarksQFListAll<cr>", desc = "list all marks" },
+      { "<leader>mb", "<cmd>BookmarksQFListAll<cr>", desc = "list all bookmarks" },
     },
   },
 }
