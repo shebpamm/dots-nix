7effd0261c924949999282aa57b889cbcf0f8692
Erik Karsten <erik@opcode.xyz>
2025-03-07 14:52:09 +0000

    world: add starship settings for aws

--- hm/shell/starship.nix
+++ hm/shell/starship.nix
@@ -1,4 +1,11 @@
 { config, pkgs, ... }:
 {
   programs.starship.enable = false;
+  programs.starship.settings = {
+    add_newline = true;
+    aws.disabled = false;
+    aws.format = "on [$symbol($profile)]($style) ";
+    aws.style = "bold yellow";
+    aws.symbol = "☁️ ";
+  };
 }
