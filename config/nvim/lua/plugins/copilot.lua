a67e3dcb5a23f83893167ba45216625832e64e8b
Erik Karsten <erik@opcode.xyz>
2025-04-07 13:07:20 +0300

    nvim: copilot.lua

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,19 @@
 return {
-  "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup {
+      suggestion = {
+        enabled = true;
+        hide_during_completion = false;
+        auto_trigger = true;
+        keymap = {
+          accept = "<C-f>",
+          prev = "<C-j>",
+          next = "<C-l>",
+        },
+      },
+    }
+  end,
 }
