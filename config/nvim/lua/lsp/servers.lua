6d93815c6488f1aa6b56c49c145a05ee83b9f242
Erik Karsten <erik@opcode.xyz>
2025-09-16 17:56:20 +0300

    nvim: nuke emmet

--- config/nvim/lua/lsp/servers.lua
+++ config/nvim/lua/lsp/servers.lua
@@ -1,32 +1,33 @@
 local servers = {
-  emmet_ls = require("lsp.lang.emmet").setup,
   lua_ls = require("lsp.lang.lua").setup,
   pyright = require("lsp.lang.python").setup,
   ts_ls = require("lsp.lang.typescript").setup,
   ansiblels = require("lsp.lang.ansible").setup,
 
-  awk_ls = {},
-  bashls = {},
-  clangd = {},
-  cssls = {},
-  dockerls = {},
-  dotls = {},
-  hls = {},
-  html = {},
-  jsonls = {},
-  nil_ls = {},
-  robotframework_ls = {},
-  ruff = {},
-  sqlls = {},
-  taplo = {},
-  tflint = {},
+  awk_ls = nil,
+  bashls = nil,
+  clangd = nil,
+  cssls = nil,
+  dockerls = nil,
+  dotls = nil,
+  hls = nil,
+  html = nil,
+  jsonls = nil,
+  nil_ls = nil,
+  robotframework_ls = nil,
+  ruff = nil,
+  sqlls = nil,
+  taplo = nil,
+  tflint = nil,
 }
 
 for name, opts in pairs(servers) do
   if type(opts) == "function" then
     opts()
   else
-    vim.lsp.config(name, opts)
+    if opts ~= nil then
+      vim.lsp.config(name, opts)
+    end
     vim.lsp.enable(name)
   end
 end
