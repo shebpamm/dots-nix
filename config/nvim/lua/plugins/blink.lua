9d41445be7ad79dfd90b1fa6e43da768054e2bc8
Erik Karsten <erik@opcode.xyz>
2025-09-16 18:15:12 +0300

    nvim: blink to use super-tab keys

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,12 +1,14 @@
 return {
   "saghen/blink.cmp",
-  build = 'cargo build --release',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  build = "cargo build --release",
+  dependencies = { "rafamadriz/friendly-snippets" },
 
   --- @module 'blink.cmp'
   --- @type blink.cmp.Config
   opts = {
-    keymap = { preset = 'default' },
-    appearance = { nerd_font_variant = 'mono' },
-  }
+    keymap = {
+      preset = "super-tab",
+    },
+    appearance = { nerd_font_variant = "mono" },
+  },
 }
