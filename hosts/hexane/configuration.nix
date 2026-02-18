4c7a66e3cdfccaceba02b8f1020a579a532889c0
Erik Karsten <erik@opcode.xyz>
2026-02-18 17:27:16 +0200

    hexane: enable ipv6

--- hosts/hexane/configuration.nix
+++ hosts/hexane/configuration.nix
@@ -76,7 +76,7 @@
   networking = {
     hostName = "hexane";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
-    enableIPv6 = false;
+    enableIPv6 = true;
     useDHCP = true;
     wireless.iwd.enable = true;
     search = [
