8e09f3c04686c2520c26464ecb56b9ce4aae57a2
Erik Karsten <erik@opcode.xyz>
2026-04-04 09:14:57 +0000

    feat(legacy): provide legacy overlays for both nixos+hm

--- modules/legacy/overlay.nix
+++ modules/legacy/overlay.nix
@@ -1,29 +1,40 @@
 { inputs, ... }:
 {
   flake.aspects = {
-    legacy = {
-      nixos = { system, ... }:
-        {
-          nixpkgs.overlays = [
-            inputs.nixpkgs-f2k.overlays.default
-            inputs.nixpkgs-sheb.overlay
-            inputs.neovim-nightly.overlays.default
-            inputs.hyprpaper.overlays.default
-            inputs.anttipkgs.overlays.default
-            (self: super: {
-              zen-browser = inputs.zen.packages.${system}.default;
-              flox = inputs.flox.packages.${system}.flox;
-              compfy = inputs.compfy.packages.${system}.compfy;
-              devenv = inputs.devenv.packages.${system}.devenv;
-              kat = inputs.kat.packages.${system}.kat;
-              nur = import inputs.nur {
-                nurpkgs = super;
-                pkgs = super;
-              };
-              cursor-latest = inputs.cursor.packages.${system}.cursor;
-            })
-          ];
+    legacy =
+      let
+        generateMiscOverlay = system: self: super: {
+          zen-browser = inputs.zen.packages.${system}.default;
+          flox = inputs.flox.packages.${system}.flox;
+          compfy = inputs.compfy.packages.${system}.compfy;
+          devenv = inputs.devenv.packages.${system}.devenv;
+          kat = inputs.kat.packages.${system}.kat;
+          nur = import inputs.nur {
+            nurpkgs = super;
+            pkgs = super;
+          };
+          cursor-latest = inputs.cursor.packages.${system}.cursor;
         };
-    };
+
+        legacyOverlays = [
+          inputs.nixpkgs-f2k.overlays.default
+          inputs.nixpkgs-sheb.overlay
+          inputs.neovim-nightly.overlays.default
+          inputs.hyprpaper.overlays.default
+          inputs.anttipkgs.overlays.default
+        ];
+
+        overlaysForSystem = system: legacyOverlays ++ [ generateMiscOverlay system ];
+      in
+      {
+        nixos = { system, ... }:
+          {
+            nixpkgs.overlays = overlaysForSystem system;
+          };
+        homeManager = { system, ... }:
+          {
+            nixpkgs.overlays = overlaysForSystem system;
+          };
+      };
   };
 }
