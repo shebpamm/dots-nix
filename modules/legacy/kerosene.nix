abcf115f43ba9712f4d090bc273b4c74901e0514
Erik Karsten <erik@opcode.xyz>
2026-04-04 08:08:31 +0000

    chore(nix): use aspects in legacy overlay

--- modules/legacy/kerosene.nix
+++ modules/legacy/kerosene.nix
@@ -1,15 +1,17 @@
-{ inputs, ...}:
+{ inputs, ... }:
 {
-  flake.modules.nixos.kerosene = {
-    imports = [
-      # external modules are imported here until legacy modules are refactored
-      inputs.sops-nix.nixosModules.sops
-      inputs.disko.nixosModules.disko
+  flake.aspects = { aspects, ... }: {
+    kerosene = {
+      includes = [ aspects.legacy ];
+      nixos = {
+        imports = [
+          # external modules are imported here until legacy modules are refactored
+          inputs.sops-nix.nixosModules.sops
+          inputs.disko.nixosModules.disko
 
-      # common legacy modules, eg. overlays
-      inputs.self.modules.nixos.legacy
-
-      ../../hosts/kerosene/configuration.nix
-    ];
+          ../../hosts/kerosene/configuration.nix
+        ];
+      };
+    };
   };
 }
