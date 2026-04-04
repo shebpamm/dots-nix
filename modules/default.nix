823dd9fdb20da6de7723501b41eadeb266605bf9
Erik Karsten <erik@opcode.xyz>
2026-04-04 10:04:52 +0000

    feat(hexane): move hm to mkHomeManager

--- modules/default.nix
+++ modules/default.nix
@@ -44,38 +44,6 @@
       ];
     in
     {
-      homemanagerConfigurations = {
-        hexane = inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = { inherit inputs context; };
-          modules = [
-            rec {
-              home = {
-                homeDirectory = "/home/${context.mainUser}";
-                username = context.mainUser;
-                stateVersion = "23.11";
-              };
-
-              nixpkgs.overlays = overlays;
-              programs.home-manager.enable = true;
-
-              programs.ssh.matchBlocks."*".extraOptions = { IdentityAgent = "~/.1password/agent.sock"; };
-              home.sessionVariables.SSH_AUTH_SOCK = "${home.homeDirectory}/.1password/agent.sock";
-            }
-            ../hm/desktop
-            ../hm/desktop/windowManagers/awesome.nix
-            ../hm/dev
-            ../hm/editors/neovim.nix
-            ../hm/editors/vim.nix
-            ../hm/editors/vscode.nix
-            ../hm/system/clipcat.nix
-            ../hm/programs
-            ../hm/programs/graphics.nix
-            ../hm/programs/xorg.nix
-            ../hm/programs/work.nix
-            ../hm/shell
-          ] ++ defaultHomeManagerModules;
-        };
-      };
+      homemanagerConfigurations = { };
     };
 }
--- modules/legacy/hexane.nix
+++ modules/legacy/hexane.nix
@@ -8,6 +8,34 @@
           ../../hosts/hexane/configuration.nix
         ];
       };
+      homeManager = {
+        imports = [
+          rec {
+            home = {
+              homeDirectory = "/home/shebpamm";
+              username = "shebpamm";
+              stateVersion = "23.11";
+            };
+
+            programs.home-manager.enable = true;
+
+            programs.ssh.matchBlocks."*".extraOptions = { IdentityAgent = "~/.1password/agent.sock"; };
+            home.sessionVariables.SSH_AUTH_SOCK = "${home.homeDirectory}/.1password/agent.sock";
+          }
+          ../../hm/desktop
+          ../../hm/desktop/windowManagers/awesome.nix
+          ../../hm/dev
+          ../../hm/editors/neovim.nix
+          ../../hm/editors/vim.nix
+          ../../hm/editors/vscode.nix
+          ../../hm/system/clipcat.nix
+          ../../hm/programs
+          ../../hm/programs/graphics.nix
+          ../../hm/programs/xorg.nix
+          ../../hm/programs/work.nix
+          ../../hm/shell
+        ];
+      };
     };
   };
 }
