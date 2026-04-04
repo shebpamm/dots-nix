b7afe3a720b7d4c11dae57e14e9f570079a32ed7
Erik Karsten <erik@opcode.xyz>
2026-04-04 09:22:14 +0000

    fix(hexane): disable homeconfig until we've moved it

--- modules/hosts/hexane/flake-parts.nix
+++ modules/hosts/hexane/flake-parts.nix
@@ -4,5 +4,5 @@
 }:
 {
   flake.nixosConfigurations = inputs.self.lib.mkNixos { system = "x86_64-linux"; name = "hexane"; };
-  flake.homeConfigurations = inputs.self.lib.mkHomeManager { system = "x86_64-linux"; name = "hexane"; };
+  # flake.homeConfigurations = inputs.self.lib.mkHomeManager { system = "x86_64-linux"; name = "hexane"; };
 }
