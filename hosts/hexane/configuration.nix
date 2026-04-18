b25927d301a865428f18527f5b3e2aa71ea4b50d
Erik Karsten <erik@opcode.xyz>
2026-04-18 15:15:32 +0300

    chore(hexane): remove duplicated fs configs, disko populates these

--- hosts/hexane/configuration.nix
+++ hosts/hexane/configuration.nix
@@ -18,34 +18,6 @@
       ../../os/games/steam.nix
     ];
 
-  fileSystems."/" =
-    {
-      device = "/dev/mapper/crypted";
-      fsType = "btrfs";
-      options = [ "subvol=@" "compress=zstd" "noatime" ];
-    };
-
-  fileSystems."/home" =
-    {
-      device = "/dev/mapper/crypted";
-      fsType = "btrfs";
-      options = [ "subvol=@home" "compress=zstd" "noatime" ];
-    };
-
-  fileSystems."/swap" =
-    {
-      device = "/dev/mapper/crypted";
-      fsType = "btrfs";
-      options = [ "subvol=@swap" ];
-    };
-
-  fileSystems."/nix" =
-    {
-      device = "/dev/mapper/crypted";
-      fsType = "btrfs";
-      options = [ "subvol=@nix" "compress=zstd" "noatime" ];
-    };
-
   services.btrbk.instances.btrbk.onCalendar = "daily";
   services.btrbk.instances.btrbk.settings = {
     snapshot_preserve = "14d";
