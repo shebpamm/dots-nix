02da39c205a8533add6a0849198dae9687a987be
Erik Karsten <erik@opcode.xyz>
2026-04-18 13:40:48 +0300

    feat(base): include gcc in all workstations

--- modules/system/workstation.nix
+++ modules/system/workstation.nix
@@ -1,14 +1,24 @@
 { ... }:
 {
-  flake.aspects = { aspects, ... }: {
-    workstation = {
-      includes = [
-        aspects.system-base
-        aspects.cross-platform
-        aspects.keyring
-        aspects.yubikey
-        aspects.fish
-      ];
+  flake.aspects =
+    { aspects, ... }:
+    {
+      workstation = {
+        includes = [
+          aspects.system-base
+          aspects.cross-platform
+          aspects.keyring
+          aspects.yubikey
+          aspects.fish
+        ];
+
+        nixos =
+          { pkgs, ... }:
+          {
+            environment.systemPackages = with pkgs; [
+              gcc
+            ];
+          };
+      };
     };
-  };
 }
