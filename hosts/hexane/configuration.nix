b10ec05082078ac70636923c15286a24eab23fce
Erik Karsten <erik@opcode.xyz>
2026-04-18 16:56:23 +0300

    feat(hexane): move disks to module

--- hosts/hexane/configuration.nix
+++ hosts/hexane/configuration.nix
@@ -5,9 +5,6 @@
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
 
-      # Disk setup
-      ./disks.nix
-
       # Other Modules
       ../../os/system
       ../../os/system/graphics/xorg.nix
