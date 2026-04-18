ad0c2778d1d14943bc306ae72b33130ece92acd3
Erik Karsten <erik@opcode.xyz>
2026-04-18 16:52:38 +0300

    feat(hexane): move backups to module

--- hosts/hexane/configuration.nix
+++ hosts/hexane/configuration.nix
@@ -18,22 +18,6 @@
       ../../os/games/steam.nix
     ];
 
-  services.btrbk.instances.btrbk.onCalendar = "daily";
-  services.btrbk.instances.btrbk.settings = {
-    snapshot_preserve = "14d";
-    snapshot_preserve_min = "2d";
-    volume = {
-      "/" = {
-        subvolume = {
-          home = {
-            snapshot_create = "always";
-          };
-        };
-        target = "/snapshots";
-      };
-    };
-  };
-
   users.users.ws1user = {
     isSystemUser = true;
     group = "ws1user";
