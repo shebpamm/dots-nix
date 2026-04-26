8dce3316302651b06d5ef61cdf1d595878d27e9e
Erik Karsten <erik@opcode.xyz>
2026-04-26 10:44:26 +0300

    feat(fmt): enable formatters in treefmt

--- modules/nix/treefmt.nix
+++ modules/nix/treefmt.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, ... }:
+{ inputs, ... }:
 {
 
   flake-file.inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
@@ -6,4 +6,52 @@
   imports = [
     inputs.treefmt-nix.flakeModule
   ];
+
+  perSystem.treefmt = {
+    programs = {
+      # Actions
+      actionlint.enable = true;
+
+      # Nix
+      deadnix.enable = true;
+      nixfmt.enable = true;
+
+      # Golang
+      gofmt.enable = true;
+      golangci-lint.enable = true;
+
+      # JavaScript / TypeScript
+      oxfmt.enable = true;
+
+      # Python
+      ruff-check.enable = true;
+      ruff-format.enable = true;
+
+      # Rust
+      rustfmt.enable = true;
+
+      # Bash
+      shellcheck.enable = true;
+      shfmt.enable = true;
+
+      # Terraform
+      terraform.enable = true;
+      hclfmt.enable = true;
+
+      # YAML
+      yamllint.enable = true;
+      yamllint.excludes = [ "secrets/*.yaml" ]; # sops encrypted files
+      yamlfmt.enable = true;
+      yamlfmt.excludes = [ "secrets/*.yaml" ];
+
+      # Misc
+      dockerfmt.enable = true; # Dockerfiles
+      fish_indent.enable = true; # Fish
+      mdformat.enable = true; # Markdown
+      rubocop.enable = true; # Ruby
+      sqlfluff.enable = true; # SQL
+      stylua.enable = true; # Lua
+      taplo.enable = true; # TOML
+    };
+  };
 }
