6ec5ede3e766380bd8c47141bb434c1429c45247
Erik Karsten <erik@opcode.xyz>
2026-05-13 22:19:22 +0300

    feat(nix): add pipe feature

--- modules/system/nix/pipes.nix
+++ modules/system/nix/pipes.nix
@@ -7,7 +7,7 @@
         nixos =
           { ... }:
           {
-            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+            nix.settings.experimental-features = [ "pipe-operators" ];
           };
         homeManager = { ... }: { };
       };
