f07e72ab5f4f029d600b54e78132a305ea94d8ce
Erik Karsten <erik@opcode.xyz>
2026-05-23 11:14:48 +0300

    fix(wireshark): use gui package, add admin to group

--- modules/features/wireshark.nix
+++ modules/features/wireshark.nix
@@ -1,13 +1,15 @@
-{ ... }:
+{ inputs, ... }:
 {
   flake.aspects =
     { ... }:
     {
       wireshark = {
         nixos =
-          { ... }:
+          { pkgs, ... }:
           {
             programs.wireshark.enable = true;
+            programs.wireshark.package = pkgs.wireshark;
+            users.extraGroups.plugdev.members = [ inputs.self.context.adminUser ];
           };
         homeManager = { ... }: { };
       };
