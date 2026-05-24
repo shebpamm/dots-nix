96443f9adb275b6defe1221627a88c4d34e626cd
Erik Karsten <erik@opcode.xyz>
2026-05-24 11:16:34 +0300

    feat(nix): remove nur packages

--- modules/inputs.nix
+++ modules/inputs.nix
@@ -8,6 +8,5 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nur.url = "github:nix-community/NUR";
   };
 }
