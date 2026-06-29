39786db3ad0f81187cfa08602ec9f1577ea55690
Erik Karsten <erik@opcode.xyz>
2026-06-29 11:19:47 +0300

    feat(aws): add completions via aws_completer

---
+++ config/fish/completions/aws.fish
@@ -0,0 +1,9 @@
+if type -q aws_completer
+	function __aws_completer
+		set -lx COMP_LINE (commandline -pc)
+		set -lx COMP_POINT (string length $COMP_LINE)
+		aws_completer | tr -d ' '
+	end
+
+	complete -c aws -f -a "(__aws_completer)"
+end
