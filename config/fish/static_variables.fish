15b6114fd35387dda63a016c0da2ac8496e48711
Erik Karsten <erik@opcode.xyz>
2026-07-01 19:51:25 +0300

    fix(fish): set nix env color

--- config/fish/static_variables.fish
+++ config/fish/static_variables.fish
@@ -2,4 +2,5 @@ set -U theme_color_scheme nord
 set -U theme_nerd_fonts yes
 set -U theme_newline_prompt \x3e\x20
 set -U theme_display_virtualenv no
+set -U color_nix 4d6fb7 dfdfdf
 set -U GRANTED_ALIAS_CONFIGURED true
