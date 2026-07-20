46aa89692c5a3e682e5447e81414a7f7b57c1476
Erik Karsten <erik@opcode.xyz>
2026-07-20 14:35:28 +0300

    feat(ssh): add admin pubkey to authorizedkeys

--- modules/constants/context.nix
+++ modules/constants/context.nix
@@ -8,6 +8,7 @@
 
   config.flake.context = rec {
     adminUser = "shebpamm";
+    adminPubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2zXSYBvkCarq9hsQmYAilLbrCaFqDaW7eV8S1nK+oC shebpamm@sorsa.cloud";
     homeDirectory = "/home/${adminUser}";
     dotfilesPath = "${homeDirectory}/dotfiles";
   };
