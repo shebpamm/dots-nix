c2ae953095bb17624e0aac7893df8d571f9ee4c9
Erik Karsten <erik@opcode.xyz>
2026-09-14 17:30:18 +0300

    feat(nvim): add emoji cmp to blink

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,11 +1,39 @@
 return {
   "saghen/blink.cmp",
   build = "cargo build --release",
-  dependencies = { "rafamadriz/friendly-snippets" },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "moyiz/blink-emoji.nvim",
+  },
 
   --- @module 'blink.cmp'
   --- @type blink.cmp.Config
   opts = {
+    sources = {
+      default = { "emoji" },
+      providers = {
+        emoji = {
+          module = "blink-emoji",
+          name = "Emoji",
+          score_offset = 15, -- Tune by preference
+          opts = {
+            insert = true, -- Insert emoji (default) or complete its name
+            ---@type string|table|fun():table
+            trigger = function()
+              return { ":" }
+            end,
+          },
+          should_show_items = function()
+            return vim.tbl_contains(
+              -- Enable emoji completion only for git commits and markdown.
+              -- By default, enabled for all file-types.
+              { "gitcommit", "markdown" },
+              vim.o.filetype
+            )
+          end,
+        },
+      },
+    },
     keymap = {
       preset = "super-tab",
     },
