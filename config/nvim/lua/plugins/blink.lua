330c9280334a77f31a2d1b861d135c1e599acb43
Erik Karsten <erik@opcode.xyz>
2026-09-23 10:33:22 +0300

    feat(nvim): bump blink, remove blink-emoji

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,37 +1,42 @@
+local emoji_provider = {
+  module = "blink-emoji",
+  name = "Emoji",
+  score_offset = 15, -- Tune by preference
+  opts = {
+    insert = true, -- Insert emoji (default) or complete its name
+    ---@type string|table|fun():table
+    trigger = function()
+      return { ":" }
+    end,
+  },
+  should_show_items = function()
+    return vim.tbl_contains(
+      -- Enable emoji completion only for git commits and markdown.
+      -- By default, enabled for all file-types.
+      { "gitcommit", "markdown" },
+      vim.o.filetype
+    )
+  end,
+}
+
 return {
   "saghen/blink.cmp",
-  build = "cargo build --release",
+  build = function()
+    require("blink.cmp").build():pwait()
+  end,
   dependencies = {
+    "saghen/blink.lib",
     "rafamadriz/friendly-snippets",
-    "moyiz/blink-emoji.nvim",
+    -- "moyiz/blink-emoji.nvim",
   },
 
   --- @module 'blink.cmp'
   --- @type blink.cmp.Config
   opts = {
     sources = {
-      default = { "emoji" },
+      -- default = { "emoji" },
       providers = {
-        emoji = {
-          module = "blink-emoji",
-          name = "Emoji",
-          score_offset = 15, -- Tune by preference
-          opts = {
-            insert = true, -- Insert emoji (default) or complete its name
-            ---@type string|table|fun():table
-            trigger = function()
-              return { ":" }
-            end,
-          },
-          should_show_items = function()
-            return vim.tbl_contains(
-              -- Enable emoji completion only for git commits and markdown.
-              -- By default, enabled for all file-types.
-              { "gitcommit", "markdown" },
-              vim.o.filetype
-            )
-          end,
-        },
+        emoji = emoji_provider,
       },
     },
     keymap = {
